58baffcdd526e124168e699575f6c343acc43c4b
AHA-EL-CAPI <a.hernandez105.uniandes.edu.co>
2015-06-03 15:53:20 -0500

    Clase del 3 de junio

--- Talleres/Taller 2/Revisión de Taller 2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/ComputoCienciasUniandes/MetodosComputacionalesLaboratorio/master/2015-V/actividades/lottery/lottery.csv
-head -1 lottery.csv
-grep '201219580' lottery.csv | sed 's/201219580,//g'
-rm lottery.csv
